9ce4f68aedd39adf1117c55f36c4392d0ad1ad13
Văn Bảo <VanBao6002@gmail.com>
2025-10-24 16:16:03 +0700

    24/10/2025

---
+++ include/appointmentManager.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include "appointment.h"
+
+class AppointmentManager{
+    private:    
+        std::unordered_map<int, std::string> log;
+        std::unordered_map<int, Appointment> AppointmentTable;
+
+    public:
+        AppointmentManager() = default;
+        ~AppointmentManager() = default;
+
+        // modifiers
+        void addAppointment(int ID_, const Appointment  &doc_);
+        void editAppointment(int ID_, const Appointment &newAppointment);
+        void removeAppointment(int ID_);
+        void changeStatus(int ID_, Appointment::Status status_);
+
+        // getters
+        const Appointment& getAppointmentByID(int ID_) const;
+        std::vector<Appointment> findAppointmentsByName(const std::string& name) const;
+        const std::unordered_map<int, Appointment>& getAllAppointments() const; 
+        const std::unordered_set<int>& getPatientsByAppointmentID(int ID_) const; 
+        const std::unordered_map<int, std::string>& getAllLog() const;
+        const std::string& getIDLog(int ID_) const;
+};
--- src/appointment.cpp
+++ src/appointment.cpp
@@ -7,6 +7,12 @@
 #include "utils.h"
 #include <string>
 
+
+void Appointment::setID(int ID_){
+    Utils::validID(ID_);
+    ID = ID_;
+}
+
 void Appointment::setDateTime(Date date_, const std::string &time_){
     Utils::validDate(date_);
     Utils::validTime(time_);
@@ -25,22 +31,31 @@ void Appointment::setDoctor(const DoctorManager& mgr, int doctorID_){
 
 void Appointment::setPatient(const PatientManager& mgr, int patientID_){
     Utils::validPatientID(mgr.getAllPatientsTable(), patientID_);
-    
+    patientID = patientID_;
+}
+
+void Appointment::setRoom(const std::string room_){
+    Utils::validRoom(room_);
+    room = room_;
 }
 
 //getters
-std::string Appointment::getDateTime(){
-
+int Appointment::getID() const{
+    return ID;
 }
 
-Appointment::Status Appointment::getStatus(){
+std::string Appointment::getDateTime() const{
+    return date.toString() + " " + time;
+}
 
+Appointment::Status Appointment::getStatus() const{
+    return status;
 }
 
 Doctor Appointment::getDoctor(const DoctorManager& mgr) const{
-
+    return mgr.getDoctorByID(doctorID);
 }
 
 Patient Appointment::getPatient(const PatientManager& mgr) const{
-
+    return mgr.getPatientByID(patientID);
 }
