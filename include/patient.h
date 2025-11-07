90980178780f2bfac51538de403cfd0782ea9940
Văn Bảo <VanBao6002@gmail.com>
2025-11-07 16:19:40 +0700

    damn

--- include/patient.h
+++ include/patient.h
@@ -1,39 +1,35 @@
 #pragma once
-
 #include "person.h"
 #include "medicalRecord.h"
 #include "prescription.h"
-#include "json.hpp"
 #include <string>
-#include <iostream>
-#include <sstream>
+#include "json.hpp"  // nlohmann/json
 
+class Patient : public Person {
+private:
+    std::string bloodType;
+    std::string nameMother;
+    std::string nameFather;
+    MedicalRecord medicalRecord;
+    Prescription prescription;
 
-class Patient : public Person{
-    private:
-        std::string bloodType;
-        std::string nameMother;
-        std::string nameFather;
-        MedicalRecord medicalRecord;
-        Prescription prescription;
-    public:
-        Patient();
+public:
+    Patient();
+    Patient(const std::string &name_, char gender_, const Date &birthday_, int ID_,
+            const std::string &bloodType_, const std::string &nameMother_, const std::string &nameFather_);
 
-        Patient(const std::string &name_, char gender_, const Date &birthday_, const std::string &bloodType, const std::string &nameMother,const std::string &nameFather);
-        
-        ~Patient();
+    virtual ~Patient() = default;
+    virtual std::string getInfo() const override;
 
-        virtual std::string getInfo() const override;
+    std::string getBloodType() const { return bloodType; }
+    std::string getNameMother() const { return nameMother; }
+    std::string getNameFather() const { return nameFather; }
 
-        std::string getBloodType() const {return bloodType;}
-        std::string getNameMother() const {return nameMother;}
-        std::string getNameFather() const {return nameFather;}
+    void setBloodType(const std::string &bloodType_);
+    void setNameMother(const std::string &nameMother_);
+    void setNameFather(const std::string &nameFather_);
 
-        void setBloodType(const std::string &bloodType_);
-        void setNameMother(const std::string &nameMother_);
-        void setNameFather(const std::string &nameFather_);
-
-        nlohmann::json toJson() const;
-        void fromJson(const nlohmann::json &j);
+    // JSON
+    nlohmann::json toJson() const;
+    void fromJson(const nlohmann::json &j);
 };
-
