e382fe4ce9d1a9f1bd2cabbedbe9d79d43f7d5b7
Văn Bảo <VanBao6002@gmail.com>
2025-11-21 20:53:57 +0700

    working on UI

--- main.cpp
+++ main.cpp
@@ -5,12 +5,23 @@
 #include <QJsonDocument>
 #include <QDebug>
 #include "gui/mainwindow.h"
+#include "core.h"
 
 int main(int argc, char *argv[])
 {
+    Core core;
+    try {
+        core.loadAll();
+    } catch (const std::exception& e) {
+        QMessageBox::critical(nullptr, "Error", QString("Failed to load data: %1").arg(e.what()));
+        return 1;
+    }
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QObject::connect(&a, &QApplication::aboutToQuit, [&core](){ core.saveAll(); });
 
     return a.exec();
 }
