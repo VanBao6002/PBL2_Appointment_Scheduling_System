63fd2852a99a5ab34f4b30c25eb970453f8c5017
VanBao <VanBao6002@gmail,com>
2025-12-13 17:37:23 +0700

    added new 2 fields : CCCD , email to both Doctor and Patient, working on appointment feature

--- tests/testUser.cpp
+++ tests/testUser.cpp
@@ -1,56 +1,10 @@
-#include "all.h"
 #include <unordered_map>
 #include <unordered_set>
 #include <iostream>
 #include <string>
 using namespace std; 
 
-void TEST_PRINT_ALL_INFO (UserManager& mgr) {
-    for (const auto& pair : mgr.getAllUsers()) {
-        cout << "ID: " << pair.second.getID() << ", Name: " << pair.second.getUsername() << endl;
-    }
-}
-
-void TEST_LOAD(UserManager& mgr, const string& path){
-    mgr.loadFromFile(path);
-}
-
-void TEST_ADD(UserManager& mgr, User& doc) {
-    mgr.addUser(doc);
-}
-
-void TEST_EDIT(UserManager& mgr, User& doc) {
-    mgr.editUser(doc.getID(), doc);
-}
-
-void TEST_REMOVE(UserManager& mgr, User& doc) {
-    mgr.removeUser(doc.getID());
-}
-
-
- s
 int main (){
-    UserManager mgr;
+    std::cout << std::hash<std::string>{}("abc") << std::endl;
+}
 
-    TEST_LOAD(mgr, Config::USER_PATH);
-
-    User d1;
-    d1.setUsername("timmy123");
-    d1.setPassword("111111111");
-    TEST_ADD(mgr, d1);
-    TEST_PRINT_ALL_INFO(mgr);
-    
-
-    d1.setUsername("timmy686868");
-    d1.setPassword("222222222");
-    TEST_EDIT(mgr, d1);
-    TEST_PRINT_ALL_INFO(mgr);
-
-
-    TEST_REMOVE(mgr, d1);
-    TEST_PRINT_ALL_INFO(mgr);
-
-
-
-    
-} 
